a9292de9ae9c68eca312fce4d96cbbe188b0d994
Aditya Sharma <adusharma2004@gmail.com>
2023-01-01 19:07:42 -0500

    meh

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'aditya sharma',
+        'title': 'WoW such a nice post', 
+        'content': 'first post',
+        'date_posted': 'April 20th 2018'
+    },
+    {
+        'author': 'Jane',
+        'title': 'Post 2', 
+        'content': 'second post ',
+        'date_posted': 'April 21st 2018'
+    }
+]
+
 @app.route("/")
 def hello():
     return render_template('home.html')
 
 @app.route("/about")
 def about():
-    return "<h1>Hi my name is aditya sharma</h1>"
-
-
+    return render_template('about.html')
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
