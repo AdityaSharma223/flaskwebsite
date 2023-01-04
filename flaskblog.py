b6ee28e2a6cb88980f8db1d05ebdc13c7e0a6633
Aditya Sharma <adusharma2004@gmail.com>
2023-01-04 17:31:48 -0500

    login and registration forms and form validation

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+import flask
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'ab3f31f23aca274a06007247400222ff'
 
 posts = [
     {
         'author': 'Aditya',
-        'title': 'Post1 ', 
+        'title': 'Post 1', 
         'content': 'first post',
         'date_posted': 'April 20, 2018',
         'tags': ['sports', 'football']
@@ -19,12 +23,25 @@ posts = [
 ]
 
 @app.route("/")
-def hello():
+def home():
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
     return render_template('about.html', title="About")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title="Register", form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title="Login", form=form)
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
